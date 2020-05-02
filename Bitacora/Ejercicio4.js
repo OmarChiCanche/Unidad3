e5ccf3705eac43f17c5c3af61a332077f6df57ac
OmarChiCanche <43392528+OmarChiCanche@users.noreply.github.com>
2020-05-01 21:28:38 -0500

    Update4

--- Bitacora/Ejercicio4.js
+++ Bitacora/Ejercicio4.js
@@ -1,5 +1,19 @@
-/*
+/* reutilizacion de expresiones comunes, en el primer codigo la suma de b+c se repite
+por lo tanto en el segundo codigo como ya se ha calculado el valor, e sera el valor de a
 */ 
 //Codigo no optimizado
+var a,b,c,d,e,f;
+a = b + c;
+d = a - d;
+e = a + c;
+f = a - d;
 
+console.log(a,d,e,f);
 //Codigo optimizado 
+var a,b,c,d,e,f;
+a = b + c;
+d = a - d;
+e = a;
+f = a - d;
+
+console.log(a,d,e,f);
