35a03d08265d16178883ee52789874758c134904
OmarChiCanche <43392528+OmarChiCanche@users.noreply.github.com>
2020-05-01 23:52:17 -0500

    Update

--- Bitacora/Ejercicio10.js
+++ Bitacora/Ejercicio10.js
@@ -1,5 +1,18 @@
-/*
+/*Optimizacion de Costos, en el primer codigo podemos apreciar que se imprimiria uno por uno
+cada numero hasta llegar al 100 y esto requiere tiempo y genera un costo, por lo tanto
+se optimiza el codigo siendo lo mas ideal un ciclo para que imprima del 1 al 100,
+teniendo asi, una serie de repeticiones.
 */ 
 //Codigo no optimizado
-
-//Codigo optimizado 
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+console.log(6);
+// ... así hasta llegar a 100
+//Codigo optimizado
+var cont;
+for ( cont=1; cont<=100; cont++) {
+    console.log(cont);
+  }
--- Bitacora/Ejercicio9.js
+++ Bitacora/Ejercicio9.js
@@ -1,5 +1,14 @@
-/*
+/*Optimizacion global, el primer codigo no tiene definido el orden de los operandos,
+el segundo ya corrige esa parte y obtenemos un codigo mas eficiente en tiempo de ejecucion
+y en el espacio de memoria.
 */ 
 //Codigo no optimizado
-
-//Codigo optimizado 
+var B,A,D,C;
+B=2*A+(A=c*d);
+Console.log("Valor = ",B);
+//Codigo optimizado
+var A,B,C,D,X;
+A=C*D;
+B=2*A;
+X= A+B;
+Console.log("Valor = ",X);
