e6e30310447bc88e3e68439458a3c7729ed7ea1e
OmarChiCanche <43392528+OmarChiCanche@users.noreply.github.com>
2020-05-01 20:43:51 -0500

    Update

--- Bitacora/Ejercicio2.js
+++ Bitacora/Ejercicio2.js
@@ -1,19 +1,39 @@
 /*
+ Metodo de reduccion por ciclos, en el codigo no optimizado constantemente en cada ciclo
+ comparamos el tamaño de los valores haciendo repetitiva cada vez que el ciclo se repita
+ en el porceso optimzado se propone que el tamaño ya se defina desde antes para poder
+ realizar el proceso en menos tiempo y sea menos los costos y asi los proceso mas eficientes
 */ 
 //Codigo no optimizado
 var values=[1,2,3,4,5];
-//FOR
           for (var i=0; i<values.length;i++){
                     process(values[i]);
           }
-//DO-WHILE
+
           var j=0;
           do {
                     process(values[j++]);
           }while (j < values.length);
-//WHILE
+
           var k=0;
           while (k < values.length){
                     process (values[k++]);
           }
+         
 //Codigo optimizado
+var values=[1,2,3,4,5];
+var length = values.length;
+
+for (var i=length;i--;){
+          process(values[i]);
+}
+
+var j=length;
+do {
+          process(values[--j]);
+}while (j);
+
+var k=length;
+while (k--){
+          process (values[k]);
+}
